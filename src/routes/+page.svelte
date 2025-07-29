acb524105a36d101506313e9c2ad8fb71a020236
Basic-beep <elliottroseemail@gmail.com>
2025-07-28 22:13:32 -0400

    Initial Commit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,4 @@
+<h1>Hello! Welcome to my website!</h1>
+<h2>My name is Elliott but you can call me BasicBirdy</h2>
+<p>I started back in 2023 with python coding,
+     and then fall of 24' I took a C++, WPF, and rpi class at my homeschool co-op</p>
