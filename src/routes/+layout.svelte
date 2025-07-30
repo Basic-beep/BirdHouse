0745ba2eb62a4dfea2c81d88976f2f580ba1451d
Basic-beep <elliottroseemail@gmail.com>
2025-07-29 23:26:30 -0400

    realeyes

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,14 @@
+
+
+<script lang="ts">
+	let { children } = $props();
+</script>
+<div style="text-align: center;">
+  <nav>
+	<a href="/"><button>Hello</button></a>
+	<a href="/about"><button>About Me</button></a>
+    <a href="/Projects"><button>My Projects</button></a>
+  </nav>
+</div>
+
+{@render children()}
---
+++ src/routes/about/+page.svelte
@@ -0,0 +1,4 @@
+<div style="text-align: center;">
+    <p>I started back in spring of 2024 with some simple python programing, <br>
+    then fall of 24' I took a C++, WPF, and rpi class at my homeschool co-op</p>
+</div>
